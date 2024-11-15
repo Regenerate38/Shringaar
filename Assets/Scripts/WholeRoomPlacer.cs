1426acdf489c66d2b642d68f051590d0633031ba
Regeneratel38 <44703575+Regeneratel38@users.noreply.github.com>
2024-11-15 20:26:23 +0545

    Added Room furniture spawner

---
+++ Assets/Scripts/WholeRoomPlacer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class WholeRoomPlacer : MonoBehaviour
+{
+    
+//    public ARRaycastManager raycastManager;
+
+    void Start()
+    {
+//        raycastManager = FindObjectOfType<ARRaycastManager>();
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
